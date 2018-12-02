df0151245e3a2f761bd2ead84a894ca7701273d3
Amber Mao <maoa@oregonstate.edu>
2018-12-01 18:42:21 -0800

    added stuff

--- public/index.js
+++ public/index.js
@@ -1,14 +1,30 @@
 var searchButton = document.getElementById("search-button");
-searchButton.addEventListener("click", searchButtonHandler);
+searchButton.addEventListener("click", searchHandler);
+
+var addPostButton = document.getElementById("insert-button");
+addPostButton.addEventListener("click", addPostModalHandler);
+
+var addPhotoModal = document.getElementById("add-photo-modal");
+var postButton = document.getElementById("post-button");
+postButton.addEventListener("click", postHandler)
+var cancelButton = document.getElementById("cancel-post-button");
+cancelButton.addEventListener("click", modalCloseHandler)
 
 var photoContainer = document.getElementById("photo-feed-container");
 photoContainer.addEventListener("click", photoHandler);
 
+var photoDetailModal = document.getElementById("photo-detail-modal");
+
 var backdrop = document.getElementById("modal-backdrop");
-backdrop.addEventListener("click", modalHandler);
+backdrop.addEventListener("click", modalCloseHandler);
+
+var toggleProfileButton = document.getElementById("toggle-profile-button");
+toggleProfileButton.addEventListener("click", toggleProfileHandler);
+
+var profileContainer = document.getElementById("profile-container");
 
 
-function searchButtonHandler(event) { //waiting for search functionality
+function searchHandler(event) { //waiting for search functionality
     console.log("search button clicked");
     var searchInput = document.getElementById("search-input").value.toUpperCase();
     var captions = document.getElementsByClassName("caption-container");
@@ -16,32 +32,42 @@ function searchButtonHandler(event) { //waiting for search functionality
   
 }
 
+function addPostModalHandler(event) {
+    backdrop.classList.toggle("hidden");
+    addPhotoModal.classList.toggle("hidden");
+    //console.log("toggle modal hidden");
+}
+
+function postHandler(event) {
+    var photoURL = document.getElementById("add-photo-url").value;
+    var caption = document.getElementById("add-photo-caption").value;
+}
+
+
+
 function photoHandler(event) {
     console.log("photo container clicked");
     if(event.target.classList.contains("image")) {
         console.log("event.target: ", event.target);
         backdrop.classList.toggle("hidden");
     }
-    var modalPhoto = Handlebars.templates.modalPhoto({
-        photoURL: event.target
-    });
-    backdrop.insertAdjacentElement("beforeend", modalPhoto); //change this line later
+    
+    
 }
 
-//this just closes the modal after clicking on the background
-function modalHandler(event) {
+//this also closes the modal after clicking on the background
+function modalCloseHandler(event) {
+    addPhotoModal.classList.toggle("hidden");
     backdrop.classList.toggle("hidden");
-   
+    document.getElementById("add-photo-url").value = "";
+    document.getElementById("add-photo-caption").value = "";
 }
 
-//need to add server functionality and store the data
-function addPost(photoURL, hearts, person, caption) {
-    var postHTML = Handlebars.templates.post({
-        photoURL: photoURL,
-        hearts: hearts,
-        person: person,
-        caption: caption
-    });
 
-    photoContainer.insertAdjacentElement("beforeend", postHTML);
+function toggleProfileHandler(event) {
+    toggleProfileButton.firstChild.classList.toggle("fa-chevron-circle-left");
+    toggleProfileButton.firstChild.classList.toggle("fa-chevron-circle-right");
+    profileContainer.classList.toggle("hidden");
+
 }
+
